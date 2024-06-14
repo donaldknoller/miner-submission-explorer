433edbae60ef1918e8b454c44ed23b375749719c
donald knoller <127629911+donaldknoller@users.noreply.github.com>
2024-06-14 18:48:53 +0000

    create initial alpha

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,21 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from subnetwork import get_data
+"""
+# Welcome to Miner Dashboard (alpha)
 
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+data = get_data()
+if data:
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.reset_index(inplace=True)
+    df.rename(columns={'index': 'Miner UID'}, inplace=True)
+    st.dataframe(df,  hide_index=True)
+    # for key, value in data.items():
+    #     st.subheader(f"{key}")
+    #     for subkey, subvalue in value.items():
+    #         st.text(f"{subkey}: {subvalue}")
+else:
+    st.write("No data available.")
